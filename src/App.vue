e705547547f2b74a85df0f9931db17db301acf54
Minimata <minimata94@gmail.com>
2024-12-05 11:49:19 +0100

    feat: added pinia and file based routing. added formatting and linting in CI.

--- src/App.vue
+++ src/App.vue
@@ -15,26 +15,24 @@ async function greet() {
 </script>
 
 <template>
+  <h1>Hello App!</h1>
+  <p>
+    <strong>Current route path:</strong> {{ $route.fullPath }}
+  </p>
+  <nav>
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/about">Go to About</RouterLink>
+  </nav>
+
   <main class="container">
     <h1 class="text-xl font-bold">Welcome to Tauri</h1>
-
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
 
     <form class="row" @submit.prevent="greet">
       <input id="greet-input" v-model="name" placeholder="Enter a name..." />
       <button type="submit">Greet</button>
     </form>
     <p>{{ mouseDefault.x }} : {{ mouseDefault.y }}</p>
+
+    <RouterView />
   </main>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
-import { createApp } from "vue";
-import './style.css'
-import App from "./App.vue";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from 'vue-router/auto-routes'
 
-createApp(App).mount("#app");
+import './style.css';
+import App from './App.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(router)
+app.use(pinia)
+app.mount('#app')
