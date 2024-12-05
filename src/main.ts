76c6fb42b11b799ffdcb3887e2379c451cf88d1e
Minimata <Minimata@users.noreply.github.com>
2024-12-05 10:49:49 +0000

    Apply automatic changes

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import { createApp } from 'vue';
-import { createPinia } from 'pinia'
-import { createRouter, createWebHistory } from 'vue-router'
-import { routes } from 'vue-router/auto-routes'
+import { createPinia } from 'pinia';
+import { createRouter, createWebHistory } from 'vue-router';
+import { routes } from 'vue-router/auto-routes';
 
 import './style.css';
 import App from './App.vue';
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
-})
-const pinia = createPinia()
-const app = createApp(App)
+	history: createWebHistory(),
+	routes,
+});
+const pinia = createPinia();
+const app = createApp(App);
 
-app.use(router)
-app.use(pinia)
-app.mount('#app')
+app.use(router);
+app.use(pinia);
+app.mount('#app');
