5d3703ed3b83d76f2cf320e055e66088f080701e
Alexandre Serex <minimata94@gmail.com>
2024-12-07 14:35:16 +0100

    feat: added form validation packages

--- src/components/NewSessionModal.vue
+++ src/components/NewSessionModal.vue
@@ -3,8 +3,25 @@
         <form method="dialog">
             <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
         </form>
-        <h3 class="text-lg font-bold">Hello!</h3>
-        <p class="py-4">Press ESC key or click outside to close</p>
+        <h3 class="text-lg font-bold">Create new session</h3>
+        <label class="form-control w-full max-w-xs">
+            <div class="label">
+                <span class="label-text">Session name</span>
+            </div>
+            <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+        </label>
+        <label class="form-control w-full max-w-xs">
+            <div class="label">
+                <span class="label-text">Date</span>
+            </div>
+            <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xs" />
+        </label>
+        <label class="form-control w-full max-w-xs">
+            <div class="label">
+                <span class="label-text">Select a folder</span>
+            </div>
+            <input type="file" class="file-input file-input-bordered w-full max-w-xs" />
+        </label>
     </div>
     <form method="dialog" class="modal-backdrop">
         <button>close</button>
