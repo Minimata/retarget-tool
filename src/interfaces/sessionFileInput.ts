68da355cb1bfe8dcf352caadce4380262dc9578c
Minimata <Minimata@users.noreply.github.com>
2024-12-17 13:39:22 +0000

    Apply automatic changes

--- src/interfaces/sessionFileInput.ts
+++ src/interfaces/sessionFileInput.ts
@@ -2,6 +2,6 @@ import { DirEntry } from '@tauri-apps/plugin-fs';
 import { Session } from '../interfaces/session';
 
 export interface SessionFileInput {
-    session: Session,
-    file: DirEntry
+	session: Session;
+	file: DirEntry;
 }
