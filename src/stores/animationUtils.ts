75090ed5079e58ad1cab0bc299ee07d256809c6a
minimata <minimata94@gmail.com>
2025-03-08 12:25:51 +0100

    Starting to edit the animations through a composable store

--- src/stores/animationUtils.ts
+++ src/stores/animationUtils.ts
@@ -1,6 +1,6 @@
-import {defineStore} from 'pinia';
-import {Animation} from "../interfaces/animation.ts";
-import { Filetype, supportedFileExtensions, availableFoldersForTypes } from '../../interfaces/filesystem.ts';
+import { defineStore } from 'pinia';
+import { Animation, AnimationStatus } from "../interfaces/animation.ts";
+import { Filetype, supportedFileExtensions, availableFoldersForTypes } from '../interfaces/filesystem.ts';
 import { DirEntry, readDir } from '@tauri-apps/plugin-fs';
 import * as path from '@tauri-apps/api/path';
 
@@ -8,12 +8,14 @@ import * as path from '@tauri-apps/api/path';
 export const useAnimationsUtils = defineStore('animationUtils', () => {
     function checkAnimationsFromTimesheet(animations: Animation[]) {
         console.log(animations);
-
+        for (const animation of animations) {
+            animation.status = AnimationStatus.OK
+        }
     }
 
     function trimAnimationFBXFiles(animations: Animation[]) {
         console.log(animations);
     }
 
-    return {checkAnimationsFromTimesheet, trimAnimationFBXFiles};
+    return { checkAnimationsFromTimesheet, trimAnimationFBXFiles };
 });
